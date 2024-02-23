fa7a113e3082c1d294b8e8dfa2d08fce61f3b167
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-02-23 17:36:48 +0200

    udating a solution for best2Scores problem

--- BestScores.java
+++ BestScores.java
@@ -11,13 +11,29 @@
     firstSecond(myArray) // {90, 87}
  */
 
+ public class BestScores {
+
+    public static void main(String[] args) {
+        int[] myArray = {84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0};
+        int[] result = firstSecond(myArray);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] firstSecond(int[] array) {
+        Arrays.sort(array); // Sort the array in ascending order
+        int n = array.length;
+        // The last two elements after sorting are the first and second best scores
+        int[] result = {array[n - 1], array[n - 2]};
+        return result;
+    }
+}
 
  /**
   * Intstructor's solution
   * 
   * import java.util.Arrays;
     import java.util.Collections;
-    
+
     public class Exercise {
         public static int[] findTopTwoScores(int[] array) {
             int firstHighest = Integer.MIN_VALUE;
